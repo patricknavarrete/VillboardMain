6cbf991e71fa9077401c2fba8c924f4c64de508e
Patrick Navarrete <patricknavarrete1998@gmail.com>
2021-11-08 03:27:33 +0800

    addcors

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+app.use(cors({
+  origin: "*"
+}))
+
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
