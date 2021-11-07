ba47278118f7f34161c754175c4d1c6d0c250917
Patrick Navarrete <patricknavarrete1998@gmail.com>
2021-11-08 03:44:06 +0800

    cors5

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(cors({
-  origin: "*"
+  origin: "*",
+  methods: ["HEAD","GET","POST","DELETE","OPTIONS","Access-Control-Allow-Methods","PATCH"]
 }))
 
 
