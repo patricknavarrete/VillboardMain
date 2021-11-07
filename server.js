af16247069698e065002b497bec1a6414cc37200
Patrick Navarrete <patricknavarrete1998@gmail.com>
2021-11-07 23:51:05 +0800

    newcors

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
-app.use(cors({
-    origin: "*"
-}))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+  });
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(routes)
