c98deadbdf479b31af5151afe0ac345236e7f290
Patrick Navarrete <patricknavarrete1998@gmail.com>
2021-11-09 03:53:07 +0800

    *

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(cors({
-  origin: "https://villboardvc.herokuapp.com",
+  origin: "*",
   methods: ["HEAD","GET","POST","DELETE","OPTIONS","Access-Control-Allow-Methods","PATCH"]
 }))
 
