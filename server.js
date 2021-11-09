246fe4affb60ef8b3737edcd976f4ad2ea4c45f8
Patrick Navarrete <patricknavarrete1998@gmail.com>
2021-11-09 23:37:49 +0800

    newcors

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ connectDB()
 
 const app = express()
 
-app.use(cors({
-  origin: "*",
-  methods: ["HEAD","GET","POST","DELETE","OPTIONS","Access-Control-Allow-Methods","PATCH"]
-}))
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+app.use(cors());
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
