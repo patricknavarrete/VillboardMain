063638c8bb9d503225631e757a10df58c5a3214b
patricknavarrete <39878429+patricknavarrete@users.noreply.github.com>
2022-06-12 20:14:32 +0800

    security headers server

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ const app = express()
 
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept","Access-Control-Allow-Credentials","Access-Control-Expose-Headers","Access-Control-Max-Age","Access-Control-Allow-Methods","Access-Control-Request-Method","Access-Control-Request-Headers","OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin", "X-Requested-With", "Content-Type", "Accept","Access-Control-Allow-Credentials","Access-Control-Expose-Headers","Access-Control-Max-Age","Access-Control-Allow-Methods","Access-Control-Request-Method","Access-Control-Request-Headers","OPTIONS");
   next();
 })
 app.use(cors());
+
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
